881dcb34abc1be3c45a253b63b7326a6f06afd97
sibijose <43006907+sibijose@users.noreply.github.com>
2023-08-03 13:41:23 +0530

    Add files via upload

---
+++ variable.tf
@@ -0,0 +1,23 @@
+variable "project" {
+  default = "ansible-terraform"
+ 
+}
+ 
+ 
+variable "environment" {
+  default = "test"
+}
+ 
+ 
+variable "region" {
+  default = "ap-south-1"
+}
+ 
+variable "instance_ami" {
+  default = "ami-0cca134ec43cf708f"
+}
+ 
+variable "instance_type" {
+  default = "t2.micro"
+}
+ 
